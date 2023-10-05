ef5c4473b751a456d88e0527169740e3a54e0a9e
abdoufekkak <abdou.fekkak66@gmail.com>
2023-10-05 19:30:26 +0200

    wert'

--- src/rote/MessageRoute.ts
+++ src/rote/MessageRoute.ts
@@ -4,13 +4,13 @@ const router = express.Router();
 import {MessageDB  } from "../repo/message";
 const userRepository = new MessageDB();
 const serviceMessage = new ServiceMessage(userRepository);
-router.get("/", (req: any, res: any) => {
-    serviceMessage
+router.get("by2users/", (req: any, res: any) => {
+    serviceMessage.getmsgby2user(req,res)
 });
 
-router.post("/", (req: any, res: any) => {
-    serviceUser.save(req, res);
-  });
+// router.post("/", (req: any, res: any) => {
+//     serviceMessage.save(req, res);
+//   });
   
   router.delete("moi/:id", (req: any, res:any) => {
     serviceMessage.suppparmoi(req, res);
@@ -19,7 +19,7 @@ router.post("/", (req: any, res: any) => {
     // serviceUser.supp(req, res);
   });
   router.put("/:id", (req:any, res:any) => {
-    serviceUser.update(req, res);
+    serviceMessage.updatemssage(req, res);
   });
 
 module.exports = router;
