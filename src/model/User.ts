99950cfbf6823fe596baa2bf22285ab5e31f4477
Salma-Raf <rksalma6@gmail.com>
2023-10-15 12:53:58 +0100

    sala

--- src/model/User.ts
+++ src/model/User.ts
@@ -3,12 +3,10 @@ export class User {
   constructor(
     public username: string,
     public email: string,
-    public age:number,
-    public adress:string,
-    public deleted:Date,
-    public id?: number,
+    public age: number,
+    public adress: string,
+    public deleted: Date,
+    public status: string,
+    public id?: number
   ) {}
 }
-
-
- 
--- src/repo/User.ts
+++ src/repo/User.ts
@@ -1,7 +1,7 @@
 import { User } from "../model/User";
 
-const pgp = require('pg-promise')();
-require('dotenv').config(); 
+const pgp = require("pg-promise")();
+require("dotenv").config();
 const dbConfig = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
@@ -11,78 +11,77 @@ const dbConfig = {
 };
 const db = pgp(dbConfig);
 export class UserRepository {
-
-    async createUser(user: User) {
-        const insertQuery = `
-          INSERT INTO users (username, email)
-          VALUES ($[username], $[email])
+  async createUser(user: User) {
+    const insertQuery = `
+          INSERT INTO users (username, email,status)
+          VALUES ($[username], $[email],$[status])
           RETURNING id
         `;
-    
-        const result = await db.one(insertQuery, user);
-    
-        return result.id;
-      }
-      async GetAllUser() {
-        const insertQuery = `
+
+    const result = await db.one(insertQuery, user);
+
+    return result.id;
+  }
+  async GetAllUser() {
+    const insertQuery = `
          select * from users order by id desc
         `;
-    
-        const datA=  await db.query(insertQuery);
-        return datA;
-      }
 
-      async getUserById(id: number) {
-        const selectQuery = `
+    const datA = await db.query(insertQuery);
+    return datA;
+  }
+
+  async getUserById(id: number) {
+    const selectQuery = `
           SELECT * FROM users
           WHERE id = $[id]
         `;
 
-     
-      
-        const user = await db.oneOrNone(selectQuery, { id });
-      
-        return user;
-      }
-      async getUserByUsername(username: string) {
-        const selectQuery = `
+    const user = await db.oneOrNone(selectQuery, { id });
+
+    return user;
+  }
+  async getUserByUsername(username: string) {
+    const selectQuery = `
           SELECT * FROM users
           WHERE username = $[username]
         `;
 
-     
-      
-        const user = await db.oneOrNone(selectQuery, { username });
-      
-        return user;
-      }
-      
-      async  getAmisByIduser(id: number){
-        const selectQuery = `
+    const user = await db.oneOrNone(selectQuery, { username });
+
+    return user;
+  }
+
+  async getAmisByIduser(id: number) {
+    const selectQuery = `
         SELECT users2.id,users2.email,users2.username, users2.age
-         ,users2,address FROM users as users1,relations,users as  users2
+         ,users2.address,users2.status FROM users as users1,relations,users as  users2
         WHERE   users1.id=relations.user_id and
          users2.id=relations.friend_id and relations.type_relation='ami' and users1.id = $[id]
       `;
-      const data=  await db.query(selectQuery,{ id: id });
-return data;
-      }
-      async updateUser(id: number, updatedUserData: User) {
-        const updateQuery = `
+    const data = await db.query(selectQuery, { id: id });
+    return data;
+  }
+  async updateUser(id: number, updatedUserData: User) {
+    const updateQuery = `
           UPDATE users
-          SET username = $[username], email = $[email], age = $[age], address = $[address]
+          SET username = $[username], email = $[email], age = $[age], address = $[address],status=$[status]
           WHERE id = $[id]
         `;
-      
-        updatedUserData.id = id; // Ajoutez également l'ID aux données mises à jour
-      
-        const result = await db.result(updateQuery, updatedUserData, (r: { rowCount: number }) => r.rowCount);
-        //number de ligne 
-        return result === 1;
-      }
-      
-      async deleteUser(id: number) {
-        const updateQuery = `
+
+    updatedUserData.id = id; // Ajoutez également l'ID aux données mises à jour
+
+    const result = await db.result(
+      updateQuery,
+      updatedUserData,
+      (r: { rowCount: number }) => r.rowCount
+    );
+    //number de ligne
+    return result === 1;
+  }
+
+  async deleteUser(id: number) {
+    const updateQuery = `
         UPDATE users
         SET deleted = NOW()
         WHERE id = $1
@@ -94,26 +93,25 @@ return data;
       (r: { rowCount: any }) => r.rowCount
     );
     return result === 1;
-      }
-      async filterUsersByAgeAddress(age: number, address: string) {
-      let  touta = ``
-      const params=[]
-      if(age ){
-        touta+='AND age = $1'
-        params.push(age)
-      }
-      if(address){
-        touta+='AND address LIKE $2'
-        params.push(`%${address}%`)
-
-      }
-        const selectQuery = `
+  }
+  async filterUsersByAgeAddress(age: number, address: string) {
+    let touta = ``;
+    const params = [];
+    if (age) {
+      touta += "AND age = $1";
+      params.push(age);
+    }
+    if (address) {
+      touta += "AND address LIKE $2";
+      params.push(`%${address}%`);
+    }
+    const selectQuery = `
           SELECT * FROM users
           WHERE deleted is null   ${touta}
         `;
-      
-        const users = await db.query(selectQuery, params);
 
-        return users;}
-      
+    const users = await db.query(selectQuery, params);
+
+    return users;
+  }
 }
